15e3c059ea1321211a158ae5209d1bd4a18d26ed
Gustavo Nielsen <gustavo.iassyl@gmail.com>
2024-11-28 21:37:04 -0300

    Implementa Lógica de Conversão de Recurso

--- app/src/main/java/ufpr/br/carteiravirtual/interfaces/AwesomeApi.java
+++ app/src/main/java/ufpr/br/carteiravirtual/interfaces/AwesomeApi.java
@@ -1,4 +1,6 @@
 package ufpr.br.carteiravirtual.interfaces;
 
 public interface AwesomeApi {
+    @GET("json/last/USD-BRL,EUR-BRL,BTC-BRL,ETH-BRL")
+    fun getCotacoes(): Call<Map<String, MoedaResponse>>
 }
